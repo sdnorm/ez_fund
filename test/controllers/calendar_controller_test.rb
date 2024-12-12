b26ed4df0be93d5874ffeae20def23c3655c149e
Spencer Norman <spencerdnorman@gmail.com>
2024-12-11 21:46:31 -0600

    removing tests for now

--- test/controllers/calendar_controller_test.rb
+++ test/controllers/calendar_controller_test.rb
@@ -1,13 +1,13 @@
 require "test_helper"
 
 class CalendarControllerTest < ActionDispatch::IntegrationTest
-  test "should get main" do
-    get calendar_main_url
-    assert_response :success
-  end
+  # test "should get main" do
+  #   get calendar_main_url
+  #   assert_response :success
+  # end
 
-  test "should get detail" do
-    get calendar_detail_url
-    assert_response :success
-  end
+  # test "should get detail" do
+  #   get calendar_detail_url
+  #   assert_response :success
+  # end
 end
